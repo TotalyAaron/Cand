76f07ddf2f2fc81c1600b727acef3d537f4b0281
TotalyAaron <anhnhd.hstcb@gmail.com>
2025-11-02 18:59:52 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -2,3 +2,7 @@ function gotoerrlist() {
   window.location.href = "https://totalyaaron.github.io/Cand/ErrorList";
   return;
 }
+function gotodownload() {
+  window.location.href = "https://totalyaaron.github.io/Cand/archive";
+  return;
+}
