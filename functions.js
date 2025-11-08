80173df6d4c2fb47ca4441598a62d350bfecde04
TotalyAaron <anhnhd.hstcb@gmail.com>
2025-11-08 11:09:46 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -29,12 +29,22 @@ function showSlide(index) {
   return;
 }
 // listeners
+const stitle = document.getElementById("stitle");
 const buttons = document.getElementById("buttons");
 const errorlb = buttons.querySelector("#ErrorButton");
 const downloadlb = buttons.querySelector("#DownloadButton");
 errorlb.addEventListener("mouseenter", () => {
   showSlide(0);
+  stitle.textContent = "Search for errors that you may encounter here.");
 });
 downloadlb.addEventListener("mouseenter", () => {
   showSlide(1);
+  stitle.textContent = "Download released builds of C& interpreters or source code here.";
+  let img = document.createElement("img");
+  img.src = "ca.png";
+  img.id = "caimgp2";
+});
+downloadlb.addEventListener("mouseleave", () => {
+  let img = document.getElementById("caimgp2");
+  if (img) img.parentNode.removeChild(img);
 });
