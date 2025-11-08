50dcee10bd4ebcb834680e7c67afd9f15e8fb733
TotalyAaron <anhnhd.hstcb@gmail.com>
2025-11-08 07:38:00 +0700

    Update bcolorselect.js

--- settings/bcolorselect.js
+++ settings/bcolorselect.js
@@ -1,1 +1,12 @@
-const classicBlueOption = document.getElementById("bcolorselect").getElementById("classicblue");
+void refreshColor() {
+  let bcolor = localStorage.getItem("bcolor");
+  document.body.style.backgroundColor = bcolor;
+  return;
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const select = document.getElementById("bcolorselect");
+  select.addEventListener("select", function() {
+    localStorage.setItem("bcolor", this.value);
+  });
+  refreshColor();
+});
