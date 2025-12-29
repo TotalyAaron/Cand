ed9fa2384a817302fff6e3575a8a405c7fdc8f05
TotalyAaron <anhnhd.hstcb@gmail.com>
2025-12-29 21:59:35 +0700

    Update database.js

--- homepage/questions/database.js
+++ homepage/questions/database.js
@@ -8,14 +8,14 @@ async function addNote(text) {
   document.getElementById("output").innerText = data.message;
 }
 async function getNote(noteid) {
-  alert("Getting note: " + noteid);
+  //alert("Getting note: " + noteid);
   document.getElementById("inputquestion").placeholder = noteid;
   const res = await fetch(`/api/notes?id=${encodeURIComponent(noteid)}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" }
   });
   const notes = await res.json();
-  alert(JSON.stringify(notes));
+  //alert(JSON.stringify(notes));
   //const data = "";
   notes.forEach(note => {
     document.getElementById("output").innerText += note.text + "\n";
